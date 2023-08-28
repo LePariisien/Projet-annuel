bc637718d574a4cb6be297b944103ec4477a9735
LePariisien <80749864+LePariisien@users.noreply.github.com>
2023-08-28 17:35:01 +0200

    ADD MAILER AND EMAIL TEMPLATE

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,9 +6,12 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\UserAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
@@ -16,7 +19,7 @@ use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 class RegistrationController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(MailerInterface $mailer, Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -34,6 +37,18 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+            $email = (new TemplatedEmail())
+                ->from('cabinet@architect.com')
+                ->to($user->getEmail())
+                ->subject('Bienvenue dans FileOrganisation')
+                ->htmlTemplate('email/welcome.html.twig')
+                ->context([
+                    'user' => $user
+                ]);
+
+
+
+            $mailer->send($email);
 
             return $userAuthenticator->authenticateUser(
                 $user,
